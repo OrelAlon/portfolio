005429c7091fb1f87724afb39b75d208201eda04
orel alon <orela231089@gmail.com>
2021-11-04 10:13:00 +0200

    component about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from './component/Home';
+
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return <div>orel ALon</div>;
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+      {/* <About />
+           <Project />
+           <Contact />
+           <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
