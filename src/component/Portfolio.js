81ada04dbf65c2f11ee9c6e6e4e439cdd8cf5379
OrelAlon <orela231089@gmail.com>
2022-08-15 17:14:46 +0300

    add project

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -7,41 +7,49 @@ import Project1 from "../img/food.png";
 import Project2 from "../img/mine.png";
 import Project3 from "../img/dice.png";
 import Project4 from "../img/finder2.png";
+import Project5 from "../img/booking2.png";
 
 // https://www.befunky.com/create/
 // 950px width
 
 function Portfolio() {
   return (
-    <div className="project component__space" id="Portfolio">
-      <div className="heading">
-        <h1 className="heading">Some Of My Project's... </h1>
+    <div className='project component__space' id='Portfolio'>
+      <div className='heading'>
+        <h1 className='heading'>Some Of My Project's... </h1>
       </div>
-      <div className="container">
-        <div className="row">
+      <div className='container'>
+        <div className='row'>
+          <Project
+            id={Project5}
+            href='https://booking-orel-app.herokuapp.com/'
+            git='https://github.com/OrelAlon/booking-app'
+            name='Booking-Clone'
+          />
+
           <Project
             id={Project1}
-            href="https://nutrition-tracking-app.netlify.app/"
-            git="https://github.com/OrelAlon/nutrition-app"
-            name="Nutrition Tracking"
+            href='https://nutrition-tracking-app.netlify.app/'
+            git='https://github.com/OrelAlon/nutrition-app'
+            name='Nutrition Tracking'
           />
           <Project
             id={Project2}
-            href="https://cocky-dijkstra-a469d1.netlify.app/"
-            git="https://github.com/OrelAlon/Minecraft-2D-Game"
-            name="Minecraft"
+            href='https://cocky-dijkstra-a469d1.netlify.app/'
+            git='https://github.com/OrelAlon/Minecraft-2D-Game'
+            name='Minecraft'
           />
           <Project
             id={Project3}
-            href="https://awesome-turing-93446d.netlify.app/"
-            git="https://github.com/OrelAlon/Dice-Game"
-            name="Dice-Game"
+            href='https://awesome-turing-93446d.netlify.app/'
+            git='https://github.com/OrelAlon/Dice-Game'
+            name='Dice-Game'
           />
           <Project
             id={Project4}
-            href="https://github-finder-umber.vercel.app/"
-            git="https://github.com/OrelAlon/github-finder"
-            name="Github-Finder"
+            href='https://github-finder-umber.vercel.app/'
+            git='https://github.com/OrelAlon/github-finder'
+            name='Github-Finder'
           />
         </div>
       </div>
