39b02da2680cccbb4eef70a83499eb72cb6c50af
OrelAlon <orela231089@gmail.com>
2022-08-30 22:28:20 +0300

    change CV

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -38,7 +38,7 @@ function Contact() {
                 <FontAwesomeIcon icon={faLinkedin} className='icon' />
               </a>
               <a
-                href='https://drive.google.com/file/d/1JIZoC32SOcUNA3SjbeIZvFnBJljZ8fqb/view?usp=sharing'
+                href='https://drive.google.com/file/d/1Fs8LAT0JB4ussaa655CRkeZRL3op_sjm/view?usp=sharing'
                 target='_blank'
               >
                 <FontAwesomeIcon icon={faFileAlt} className='icon' />
